9b6e92c3eb7e3c7a05c627c10f5aa73bfff322a7
Purabi <purbichoudhury06966@gmail.com>
2023-12-07 22:10:45 +0530

    Only employee login-signup form created

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$dbHost = 'localhost';
+$dbName = 'employee_db';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbc=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName);
+
+?>
---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Employee Login</title>
+   <link rel="stylesheet" href="bootstrap.css">
+   </style>
+</head>
+<body class="background" style="background-color: #b3d9d9;">
+  <div class="container  vh-100">
+    <div class="row justify-content-center h-100">
+      <div class="card w-25 my-auto shadow rounded">
+        <div class="card-header text-center bg-info text-white">
+          Employee Login
+        </div>
+        <div class="card-body">
+          <form action="" method="">
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="username" id="username" class="form-control rounded" name=""/>
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" id="password" class="form-control rounded" name="" />
+            </div>
+          <input type="submit" class="btn btn-primary w-100 bg-info text-white" value="Login" name="">
+          <div class="footer">New User? <a href="signup.php">Sign up</a>
+        </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
