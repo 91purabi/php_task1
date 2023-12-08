6ccb819b33af559cf6fdc5087a40f5297d5bc27f
Purabi <purbichoudhury06966@gmail.com>
2023-12-08 23:07:33 +0530

    Validation added

--- signup.php
+++ signup.php
@@ -6,54 +6,46 @@
     <title>Signup Here</title>
     <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <h1 class="mt-5 text-center">Register</h1>
-    <div class="container">
-        <!-- <?php
-         if (isset($_POST["submit"])) {
-            $fullName = $_POST["fullname"];
-            $email = $_POST["email"];
-            $password = $_POST["password"];
-            $confirmpassword = $_POST["confirm_password"];
-            $error = array();
-            if (empty($fullName) OR empty($email) OR empty($password) OR empty($confirmpassword)) {
-                array_push($errors,"All fields are required");
-            }
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                array_push($errors, "Email is not valid");
-            }
-            if(strlen($password)<8){
-                array_push($errors,"Password must be at least 8 characters long");
-            }
-            if($password!==$confirmpassword){
-                array_push($errors,"Password does not match");
+    <script>
+        function validateForm() {
+            var fullname = document.forms["signupForm"]["fullname"].value;
+            var email = document.forms["signupForm"]["email"].value;
+            var password = document.forms["signupForm"]["password"].value;
+            var confirm_password = document.forms["signupForm"]["confirm_password"].value;
+
+            if (fullname === "" || email === "" || password === "" || confirm_password === "") {
+                alert("All fields must be filled out");
+                return false;
             }
 
-            if (count($errors)>0) {
-                foreach ($errors as $error){
-                    echo "<div class='alert alert-danger'></div>";
-                }
-            }else{
-                //We will insert the data into database
+            if (password !== confirm_password) {
+                alert("Passwords do not match");
+                return false;
             }
-         }
-        ?> -->
-        <form action="signup.php" method="post">
-            <div class="form-group">
-                <input type="text" class="form-control" name="fullname" placeholder="Full Name:">
+
+            return true;
+        }
+    </script>
+</head>
+<body>
+    <div class="container">
+        <form name="signupForm" action="signup.php" method="post" onsubmit="return validateForm()" class="bg-light p-4 rounded">
+            <h2 class="text-center mb-4">Register</h2>
+            <div class="mb-3">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name:" required>
             </div>
-            <div class="form-group">
-                <input type="email" class="form-control" name="email" placeholder="Email:">
+            <div class="mb-3">
+                <input type="email" class="form-control" name="email" placeholder="Email:" required>
             </div>
-            <div class="form-group">
-                <input type="text" class="form-control" name="password" placeholder="Password:">
+            <div class="mb-3">
+                <input type="password" class="form-control" name="password" placeholder="Password:" required>
             </div>
-            <div class="form-group">
-                <input type="text" class="form-control" name="confirm_password" placeholder="Confirm Password:">
+            <div class="mb-3">
+                <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password:" required>
             </div>
             <div class="form-btn">
-                <input type="submit" class="btn btn-primary" name="Register" placeholder="Submit">Already have an account?<a href="login.php">Login Here</a>
+                <button type="submit" class="btn btn-primary w-100 bg-info text-white">Register</button>
+                <p class="mt-3">Already have an account? <a href="login.php">Login Here</a></p>
             </div>
         </form>
     </div>
